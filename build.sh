483c66d1f84b9e29fd22f84763c93f94a27cedf8
Cristi Constantin <cristi.constantin@sent.com>
2019-07-18 23:48:41 +0100

    Updated the landing page

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-git checkout master
 hugo --verbose
 rm -r public/categories/
 rm -r public/tags/
 rm -r public/readme/
 node compress.js
 
-# git checkout gh-pages
-# rsync -var public/ .
+# rsync --archive --compress --verbose --progress public/ ../siteHTML
